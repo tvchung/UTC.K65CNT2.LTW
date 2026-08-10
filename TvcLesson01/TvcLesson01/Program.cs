cd5e151678547057c5a9926f69a2d94aa055ab8f
tvchung <hna.tvchung@gmmail.com>
2026-08-10 10:41:43 +0700

    Lesson01

---
+++ TvcLesson01/TvcLesson01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TvcLesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AspNetCore MVC - UTC.K65CNT2.LTW");
+        }
+    }
+}
