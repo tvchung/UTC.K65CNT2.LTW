a69ce822b6d695fe2bf734b46e0afb4d1646b1db
tvchung <hna.tvchung@gmmail.com>
2026-08-17 10:34:33 +0700

    TvcLesson02

---
+++ TvcLesson02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TvcLesson02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello, Chung Trịnh!");
+
+            app.Run();
+        }
+    }
+}
